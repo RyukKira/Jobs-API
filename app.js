061a11ae65d74c7e0c2cada87da84277899683cb
RyukKira <tursunov_khudoyberdi@mail.ru>
2023-05-08 21:10:47 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+const express = require("express");
+const app = express();
+
+// connectDB
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+// routes
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
+
+const swaggerUi = require("swagger-ui-express");
+const Yaml = require("yamljs");
+const swaggerDocument = Yaml.load("./swagger.yaml");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.send('<h1>Jobs Api</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
